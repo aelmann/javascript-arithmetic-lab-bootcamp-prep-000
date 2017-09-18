fe9f5f1db4aaa244901f4eda78c43f4188e4a884
Ariel Elmann <ariel.j.elmann@gmail.com>
2017-09-18 19:38:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function add(a,b) {
-  return parseInt(a+b) }
+function add(a, b) {
+  return parseInt(a + b) }
+
+function subtract(a, b) {
+  return parseInt(a - b)}
+
+function multiply(a, b) {
+  return parseInt(a * b)}
+
+function divide(a, b) {
+  return parseInt(a / b)}
+
+function inc(n) {
+  return parseInt(n++)
+}
+function dec(n) {
+  return parseInt(n--)
+}
